890a7344475c5b122a8efc0b0a2a51de6ed32b99
Sebastian Reinhard <sebastian@sebbert.me>
2019-12-07 19:56:33 +0100

    Use std::iter::once for program input

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -1,5 +1,6 @@
 use env_logger;
 use intcode::*;
+use std::iter;
 
 fn main() {
   env_logger::init();
@@ -8,13 +9,13 @@ fn main() {
   let program = parse_program(program_src);
 
   let part1 = {
-    let vm = VM::new(program.clone(), [1 as isize].iter().cloned());
+    let vm = VM::new(program.clone(), iter::once(1));
     vm.run()
   };
   println!("Part 1: {:?}", part1);
 
   let part2 = {
-    let vm = VM::new(program.clone(), [5 as isize].iter().cloned());
+    let vm = VM::new(program.clone(), iter::once(5));
     vm.run()
   };
   println!("Part 2: {:?}", part2);
